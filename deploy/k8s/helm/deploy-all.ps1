48cb900be672699168085e9a6abd31344a68d50d
Chris Witte <chwitt@microsoft.com>
2021-03-18 20:51:48 -0500

    Add debug message

--- deploy/k8s/helm/deploy-all.ps1
+++ deploy/k8s/helm/deploy-all.ps1
@@ -29,6 +29,7 @@ function Install-Chart  {
         }
     }
     if ($customRegistry) {
+        Write-Host "Using custom registry $registry, $dockerUser"
         $options = "$options --set inf.registry.server=$registry --set inf.registry.login=$dockerUser --set inf.registry.pwd=$dockerPassword --set inf.registry.secretName=eshop-docker-scret"
     }
     
